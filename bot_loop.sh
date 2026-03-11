228170771d69e5b91dbe79d777453d2184d4e9bd
matangkuwago <marlon.hacla@gmail.com>
2026-03-11 19:25:47 +0800

    Make bot run every 25 minutes.

--- bot_loop.sh
+++ bot_loop.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+./run_bot.sh
+
+limit_seconds=1500
+start_time=$SECONDS
+
 while true; do
-    ./run_bot.sh
-    for (( seconds=1500; seconds>0; seconds-- )); do
-        minutes=$(echo "scale=2; $seconds/60" | bc)
+    elapsed_time=$(($SECONDS - $start_time))
+    if [ "$elapsed_time" -ge "$limit_seconds" ]; then
+        ./run_bot.sh
+        start_time=$SECONDS
+    else
+        remaining_seconds=$(($limit_seconds - $elapsed_time))
+        minutes=$(echo "scale=2; $remaining_seconds/60" | bc)
         echo "$minutes minute(s) remaining till the next run."
-        sleep 1
-    done
+        python update_trade_records.py
+    fi
+    sleep 1
 done
