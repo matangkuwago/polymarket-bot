dd15117baadd2357a4e5b21fa35d861e78db0227
matangkuwago <marlon.hacla@gmail.com>
2026-03-13 08:11:27 +0800

    Fix bot loop.

--- bot_loop.sh
+++ bot_loop.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-./run_bot.sh
 
-limit_seconds=1340
 start_time=$SECONDS
+limit_seconds=1500
+
+./run_bot.sh
 
 while true; do
     elapsed_time=$(($SECONDS - $start_time))
     if [ "$elapsed_time" -ge "$limit_seconds" ]; then
-        ./run_bot.sh
         start_time=$SECONDS
+        ./run_bot.sh
     else
         remaining_seconds=$(($limit_seconds - $elapsed_time))
         minutes=$(echo "scale=2; $remaining_seconds/60" | bc)
