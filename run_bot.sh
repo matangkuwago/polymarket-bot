aa328fb88eede53b822476ee2176e3dd4db78526
matangkuwago <marlon.hacla@gmail.com>
2026-03-14 01:46:51 +0800

    Use env for paper trade settings.

--- run_bot.sh
+++ run_bot.sh
@@ -14,17 +14,19 @@ trap 'rm -rf $LOCK_FILE' EXIT  # remove the lock file on exit
 # Prediction proper here
 touch $LOCK_FILE
 
+source .env
+
 # Bot for SOL 5-minute market
-python run_bot.py --market_slug_prefix=sol-updown-5m --binance_ticker=SOLUSDT --paper_trade=false
+python run_bot.py --market_slug_prefix=sol-updown-5m --binance_ticker=SOLUSDT --paper_trade=$SOL_PAPER_TRADE
 
 # Bot for XRP 5-minute market
-python run_bot.py --market_slug_prefix=xrp-updown-5m --binance_ticker=XRPUSDT --paper_trade=false
+python run_bot.py --market_slug_prefix=xrp-updown-5m --binance_ticker=XRPUSDT --paper_trade=$XRP_PAPER_TRADE
 
 # Bot for ETH 5-minute market
-python run_bot.py --market_slug_prefix=eth-updown-5m --binance_ticker=ETHUSDT --paper_trade=false
+python run_bot.py --market_slug_prefix=eth-updown-5m --binance_ticker=ETHUSDT --paper_trade=$ETH_PAPER_TRADE
 
 # Bot for BTC 5-minute market
-python run_bot.py --market_slug_prefix=btc-updown-5m --binance_ticker=BTCUSDT --paper_trade=false
+python run_bot.py --market_slug_prefix=btc-updown-5m --binance_ticker=BTCUSDT --paper_trade=$BTC_PAPER_TRADE
 
 # Run other tasks
 python update_trade_records.py
