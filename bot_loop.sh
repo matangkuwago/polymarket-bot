77cb61b0b4234050a6fc517f58cccb5b2193b44f
matangkuwago <marlon.hacla@gmail.com>
2026-03-18 14:46:55 +0800

    Add timeout to scripts.

--- bot_loop.sh
+++ bot_loop.sh
@@ -2,13 +2,13 @@
 
 limit_seconds=1500
 start_time=$SECONDS
-./run_bot.sh
+timeout 120 ./run_bot.sh
 
 while true; do
     elapsed_time=$(($SECONDS - $start_time))
     if [ "$elapsed_time" -ge "$limit_seconds" ]; then
         start_time=$SECONDS
-        ./run_bot.sh
+        timeout 120 ./run_bot.sh
     else
         remaining_seconds=$(($limit_seconds - $elapsed_time))
         minutes=$(echo "scale=2; $remaining_seconds/60" | bc)
