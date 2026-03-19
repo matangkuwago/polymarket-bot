0aa0d5fc2969cfbf87d044d3c9669c282086a59e
matangkuwago <marlon.hacla@gmail.com>
2026-03-19 15:50:23 +0800

    Fix bug in record ordering.

--- get_stats_per_hour.py
+++ get_stats_per_hour.py
@@ -8,6 +8,8 @@ def main():
     trade_stats_data = {}
     trade_stats = TradeStats()
     trade_files = trade_stats.get_trade_files()
+    trade_files = sorted(
+        trade_files, key=lambda x: x["timestamp"], reverse=True)
     for trade_item in trade_files:
         timestamp = trade_item["timestamp"]
         trade = trade_item["trade"]
@@ -16,7 +18,11 @@ def main():
         hour = date_object.hour
         date_string = date_object.strftime("%Y-%m-%d")
         if date_string not in trade_stats_data:
-            trade_stats_data[date_string] = {x: {} for x in range(0, 24)}
+            trade_stats_data[date_string] = {x: {} for x in range(0, hour+1)}
+        else:
+            new_value = {x: {} for x in range(
+                0, hour+1)}
+            trade_stats_data[date_string] = new_value | trade_stats_data[date_string]
         for _hour in trade_stats_data[date_string].keys():
             if _hour >= hour:
                 if market_slug not in trade_stats_data[date_string][_hour]:
@@ -28,7 +34,6 @@ def main():
                 if trade.won:
                     trade_stats_data[date_string][_hour][market_slug]["num_won"] += 1
 
-    # print(json.dumps(trade_stats_data, indent=4))
     headers = ["time", "btc", "eth", "xrp", "sol"]
     table_data = []
     dates = list(trade_stats_data.keys())
@@ -46,7 +51,7 @@ def main():
                 wins = trade_stats_data[_date][_hour][market_slug]["num_won"]
                 if not count:
                     continue
-                percent = f"{float(wins / count) * 100:.2f}"
+                percent = f"{float(wins / count) * 100:.2f}%"
                 _row_data.append(percent)
             if _row_data:
                 table_data.append([date_string] + _row_data)
