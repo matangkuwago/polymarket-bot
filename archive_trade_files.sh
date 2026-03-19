44188d14f44e85e435dfe41d60702c586ea31a82
matangkuwago <marlon.hacla@gmail.com>
2026-03-19 22:39:44 +0800

    Remove reset paper trade settings script.

--- archive_trade_files.sh
+++ archive_trade_files.sh
@@ -4,5 +4,3 @@ mkdir -p old_trades/trades
 mkdir -p old_trades/trades_processed
 mv trades/*.trade old_trades/trades/
 mv trades_processed/*.trade old_trades/trades_processed/
-
-python reset_paper_trade_settings.py
