9b644a0f686ec375e4d52d6e951d9da175234344
matangkuwago <marlon.hacla@gmail.com>
2026-03-20 22:09:13 +0800

    Suppress mv errors..

--- archive_trade_files.sh
+++ archive_trade_files.sh
@@ -2,5 +2,5 @@
 
 mkdir -p old_trades/trades
 mkdir -p old_trades/trades_processed
-mv trades/*.trade old_trades/trades/
-mv trades_processed/*.trade old_trades/trades_processed/
+mv -f trades/*.trade old_trades/trades/ 2>/dev/null
+mv -f trades_processed/*.trade old_trades/trades_processed/ 2>/dev/null
