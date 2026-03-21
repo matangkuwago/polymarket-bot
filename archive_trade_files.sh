31c39bc5ad5a6ab8c5d52f2590989fed56de5a1b
matangkuwago <marlon.hacla@gmail.com>
2026-03-21 18:56:48 +0800

    Archive only processed trades.

--- archive_trade_files.sh
+++ archive_trade_files.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p old_trades/trades
 mkdir -p old_trades/trades_processed
-mv -f trades/*.trade old_trades/trades/ 2>/dev/null
 mv -f trades_processed/*.trade old_trades/trades_processed/ 2>/dev/null
