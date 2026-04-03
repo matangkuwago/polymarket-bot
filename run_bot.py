1bb7ccdd4765f1dde6332d9515260f7e6a45d54b
matangkuwago <marlon.hacla@gmail.com>
2026-04-03 19:25:56 +0800

    Script cleanup.

--- run_bot.py
+++ run_bot.py
@@ -5,11 +5,10 @@ from core.bot import Polymarket5MinuteBot
 
 parser = argparse.ArgumentParser(description="A simple Polymarket bot.")
 parser.add_argument("--market_slug_prefix", type=str, required=True,
-                    help="Polymarket 5-minute market slug prefix e.g. btc-updown-5m for BTC 5-minute market")
-parser.add_argument("--binance_ticker", type=str, required=True,
-                    help="The Binance ticker where the price will be compared e.g. BTCUSD")
+                    help="Polymarket 5-minute market slug prefix e.g. btc-updown-5m for the BTC 5-minute market")
 args = parser.parse_args()
 
+market_slug_prefix = args.market_slug_prefix
 bot = Polymarket5MinuteBot(
     polymarket_slug_prefix=args.market_slug_prefix,
     binance_ticker=args.binance_ticker
--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-start_time=$SECONDS
-
 LOCK_FILE=/tmp/polymarket_bot_lock.txt
 if [ -f $LOCK_FILE ]; then
    echo "File $LOCK_FILE exists."
@@ -12,22 +10,26 @@ fi
 trap 'rm -rf $LOCK_FILE' EXIT  # remove the lock file on exit
 touch $LOCK_FILE
 
-./process_trade_records.sh
 
-# Bot for SOL 5-minute market
-python run_bot.py --market_slug_prefix=sol-updown-5m
+limit_seconds=1500
+start_time=$SECONDS
+first_run=true
 
-# Bot for XRP 5-minute market
-python run_bot.py --market_slug_prefix=xrp-updown-5m
+declare -a tickers=("btc" "eth" "xrp" "sol")
 
-# Bot for BTC 5-minute market
-python run_bot.py --market_slug_prefix=btc-updown-5m
+while true; do
+   elapsed_time=$(($SECONDS - $start_time))
+   if [ "$elapsed_time" -ge "$limit_seconds" ] || [ "${first_run}" == "true" ]; then
+      start_time=$SECONDS
+      first_run=false
 
-# Bot for ETH 5-minute market
-python run_bot.py --market_slug_prefix=eth-updown-5m
-
-./process_trade_records.sh
-./send_stats.sh
-
-elapsed_time=$(($SECONDS - $start_time))
-echo "Elapsed time: $elapsed_time"
+      for ticker in "${tickers[@]}"; do
+         timeout 120 python run_bot.py --market_slug_prefix=${ticker}-updown-5m
+      done
+   else
+      remaining_seconds=$(($limit_seconds - $elapsed_time))
+      minutes=$(echo "scale=2; $remaining_seconds/60" | bc)
+      echo "$minutes minute(s) remaining till the next run."
+   fi
+   sleep 1
+done
