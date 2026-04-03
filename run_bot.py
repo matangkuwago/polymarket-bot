7314c858ccec6a2d0cbc77afe881df084315d03d
matangkuwago <marlon.hacla@gmail.com>
2026-04-03 20:02:05 +0800

    Remove the binance ticker argument.

--- run_bot.py
+++ run_bot.py
@@ -9,7 +9,6 @@ parser.add_argument("--market_slug_prefix", type=str, required=True,
 args = parser.parse_args()
 
 bot = Polymarket5MinuteBot(
-    polymarket_slug_prefix=args.market_slug_prefix,
-    binance_ticker=args.binance_ticker
+    polymarket_slug_prefix=args.market_slug_prefix
 )
 asyncio.run(bot.run())
