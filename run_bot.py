2123b05612e61a54ee941c52c2f16444ec3b34bc
matangkuwago <marlon.hacla@gmail.com>
2026-04-03 19:53:48 +0800

    Remove the binance ticker argument.

--- run_bot.py
+++ run_bot.py
@@ -6,8 +6,6 @@ from core.bot import Polymarket5MinuteBot
 parser = argparse.ArgumentParser(description="A simple Polymarket bot.")
 parser.add_argument("--market_slug_prefix", type=str, required=True,
                     help="Polymarket 5-minute market slug prefix e.g. btc-updown-5m for BTC 5-minute market")
-parser.add_argument("--binance_ticker", type=str, required=True,
-                    help="The Binance ticker where the price will be compared e.g. BTCUSD")
 args = parser.parse_args()
 
 bot = Polymarket5MinuteBot(
