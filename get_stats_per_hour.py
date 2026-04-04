1016a3a6a38e5cc7c1039073a32cc46cf7c3885d
matangkuwago <marlon.hacla@gmail.com>
2026-04-04 20:06:36 +0800

    Yet another update to stats per hour script *sighs*.

--- get_stats_per_hour.py
+++ get_stats_per_hour.py
@@ -3,10 +3,14 @@ from datetime import datetime
 from core.trader import TradeStats
 
 
-def main():
+def main(ticker: str):
     trade_stats_data = {}
     trade_stats = TradeStats()
     trade_files = trade_stats.get_trade_files()
+    trade_files = list(filter(
+        lambda x: ticker in x["trade"].market_slug,
+        trade_files
+    ))
 
     if not trade_files:
         print(f"No data to process.")
@@ -15,21 +19,27 @@ def main():
     trade_files = sorted(
         trade_files, key=lambda x: x["timestamp"])
 
-    record_count = 0
-    wins = 0
-    matched = 0
-    matched_wins = 0
+    previous_trade_date_text = ""
     for trade_item in trade_files:
         timestamp = trade_item["timestamp"]
         trade = trade_item["trade"]
-        hour = datetime.fromtimestamp(timestamp).hour
-        if hour not in trade_stats_data:
-            trade_stats_data[hour] = {
-                "record_count": 0,
-                "wins": 0,
-                "matched": 0,
-                "matched_wins": 0,
-            }
+        trade_date = datetime.fromtimestamp(timestamp)
+        trade_date_text = trade_date.strftime("%Y-%m-%d")
+        if previous_trade_date_text != trade_date_text:
+            record_count = 0
+            wins = 0
+            matched = 0
+            matched_wins = 0
+        hour = trade_date.hour
+        if trade_date_text not in trade_stats_data:
+            trade_stats_data[trade_date_text] = {}
+            for _hour in range(0, 24):
+                trade_stats_data[trade_date_text][_hour] = {
+                    "record_count": 0,
+                    "wins": 0,
+                    "matched": 0,
+                    "matched_wins": 0,
+                }
         record_count += 1
         if trade.won:
             wins += 1
@@ -37,28 +47,28 @@ def main():
             matched += 1
             if trade.won:
                 matched_wins += 1
-        trade_stats_data[hour]["record_count"] = record_count
-        trade_stats_data[hour]["wins"] = wins
-        trade_stats_data[hour]["matched"] = matched
-        trade_stats_data[hour]["matched_wins"] = matched_wins
+        trade_stats_data[trade_date_text][hour]["record_count"] = record_count
+        trade_stats_data[trade_date_text][hour]["wins"] = wins
+        trade_stats_data[trade_date_text][hour]["matched"] = matched
+        trade_stats_data[trade_date_text][hour]["matched_wins"] = matched_wins
+        previous_trade_date_text = trade_date_text
 
-    headers = ["time", "performance (all)", "performance (matched)",
-               "record_count", "wins", "matched", "matched_wins"]
-    csv_data = [headers,]
+    dates = list(sorted(trade_stats_data.keys()))
+    headers = ["time"] + dates
+    csv_data = [headers]
 
-    for hour in sorted(trade_stats_data.keys()):
-        record_count = trade_stats_data[hour]["record_count"]
-        wins = trade_stats_data[hour]["wins"]
-        matched = trade_stats_data[hour]["matched"]
-        matched_wins = trade_stats_data[hour]["matched_wins"]
+    for hour in range(0, 24):
         hour_string = f" {hour:02d}"
-        performance_all = f"{float(wins/record_count)*100:.2f}%" if record_count > 0 else "N/A"
-        performance_matched = f"{float(matched_wins/matched)*100:.2f}%" if matched > 0 else "N/A"
-        _row_data = [hour_string, performance_all, performance_matched,
-                     record_count, wins, matched, matched_wins]
+        performance_data = []
+        for date in dates:
+            record_count = trade_stats_data[date][hour]["record_count"]
+            wins = trade_stats_data[date][hour]["wins"]
+            performance = f"{(wins / record_count)*100:.2f}%" if record_count > 0 else ""
+            performance_data.append(performance)
+        _row_data = [hour_string] + performance_data
         csv_data.append(_row_data)
 
-    file_csv = "stats_per_hour.csv"
+    file_csv = f"stats_per_hour-{ticker}.csv"
     with open(file_csv, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(csv_data)
@@ -66,4 +76,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    for ticker in ("btc", "eth", "xrp", "sol"):
+        main(ticker)
